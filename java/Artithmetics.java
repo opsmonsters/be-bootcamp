343447cdaeb81b3d7f86b08dd0abc6e474080c26
Gershon Kumar <gershonkumar@live.com>
2024-10-14 09:41:01 +0530

    arithmetics

---
+++ java/Artithmetics.java
@@ -0,0 +1,30 @@
+
+class Arithmetics {
+
+    public static void main(String[] args) {
+        //Add
+        int a = 45 + 77;
+
+        //Sub
+        int b = 77 - 45;
+
+        //Mutliply
+        int c = 77 * 45;
+
+        //division
+        double d = 77 / 45;
+
+        //modulo (Remainder)
+        double e = 77 % 45;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+        //BODMAS -> Brackets, Division, Mutliplication, Addition, Subraction
+        int x = (15 + 40) - 54 * (20 / 10);
+        System.out.println(x);
+    }
+}

38dc05130bb28b1e945aeb712c74ec78b0537f4f
Gershon Kumar <gershonkumar@live.com>
2024-10-15 09:31:00 +0530

    short circuit

---
+++ java/ShortCircuit.java
@@ -0,0 +1,13 @@
+
+class ShortCircuit {
+
+    public static void main(String[] args) {
+        boolean a = false;
+        boolean b = false;
+
+        System.out.println(a && b);
+        System.out.println(a || b);
+
+    }
+
+}

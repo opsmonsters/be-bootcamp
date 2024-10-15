0dbf812d0fd614de11dfa9184d5a5d9b0981e9b6
Gershon Kumar <gershonkumar@live.com>
2024-10-15 08:56:03 +0530

    Arithmetic Compound

---
+++ java/ArithmeticCompound.java
@@ -0,0 +1,25 @@
+
+class ArithmeticCompound {
+
+    public static void main(String[] args) {
+        // shorthand for addition
+        int a = 20;
+        a += 15; // this is same as a = a + 15
+        System.out.println(a);
+
+        // short hand for subtraction
+        int b = 20;
+        b -= 15; // this is same as b = b - 15
+        System.out.println(b);
+
+        // short hand for multiplication
+        int c = 20;
+        c *= 2; // this is same as c = c * 2
+        System.out.println(c);
+
+        int d = 20;
+        d /= 2; //this is same as d = d / 2
+        System.out.println(d);
+
+    }
+}

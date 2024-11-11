b90dcefe26035650967fc0308f2de39188bc119e
Gershon Kumar <gershonkumar@live.com>
2024-11-11 09:50:09 +0530

    index url to function name map

--- springboot/HelloWorld/src/main/java/com/opsmonsters/HelloWorld/HomeController.java
+++ springboot/HelloWorld/src/main/java/com/opsmonsters/HelloWorld/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
     @GetMapping("/")
-    public User home(){
+    public User index(){
         User user = new User();
         user.userId = 20;
         user.lastLogin = "2 Days ago";

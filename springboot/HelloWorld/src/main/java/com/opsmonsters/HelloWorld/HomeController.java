a24aaaae97727c67b629d5e0e65d8834940ffaf9
Gershon Kumar <gershonkumar@live.com>
2024-11-11 09:49:11 +0530

    spring boot init

---
+++ springboot/HelloWorld/src/main/java/com/opsmonsters/HelloWorld/HomeController.java
@@ -0,0 +1,19 @@
+package com.opsmonsters.HelloWorld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public User home(){
+        User user = new User();
+        user.userId = 20;
+        user.lastLogin = "2 Days ago";
+        user.password = "password";
+        user.username = "Gershon";
+
+        return user;
+    }
+
+}
---
+++ springboot/HelloWorld/src/main/java/com/opsmonsters/HelloWorld/User.java
@@ -0,0 +1,8 @@
+package com.opsmonsters.HelloWorld;
+
+public class User {
+    public int userId;
+    public String username;
+    public String password;
+    public String lastLogin;
+}
